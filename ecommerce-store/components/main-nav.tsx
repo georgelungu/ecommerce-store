90f3a9765d8b4a5854d00fc43fd7bc599f746af7
George Lungu <george.lungu1896@gmail.com>
2023-10-05 21:11:59 +0300

    done the fetch for main nav

--- ecommerce-store/components/main-nav.tsx
+++ ecommerce-store/components/main-nav.tsx
@@ -2,10 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils";
+import { Category } from "@/types";
 
 interface MainNavProps {
-    data: any
+    data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({
@@ -24,16 +26,16 @@ const MainNav: React.FC<MainNavProps> = ({
         <nav
             className="mx-6 flex items-center space-x-4 lg:space-x-6"
         >
-            {routes.map(route => (
+            {routes.map((route) => (
                 <Link
                     key={route.href}
                     href={route.href}
                     className={cn(
                         "text-sm font-medium transition-colors hover:text-black",
-                        route.active? "text-black" : "text-neutral-500"
+                        route.active ? "text-black" : "text-neutral-500"
                     )}
                 >
-                
+                    {route.label}
                 </Link>
             ))}
         </nav>
--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -1,1 +1,11 @@
-// left at 07:41:40
+export interface Billboard {
+    id: string;
+    name: string;
+    imageUrl: string;
+};
+
+export interface Category {
+    id: string;
+    name: string;
+    billboard: Billboard;
+}
