7cf9137a542a87f147f420680f04bb1821553569
George Lungu <george.lungu1896@gmail.com>
2023-10-06 22:41:05 +0300

    related items done and further work on the product page

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-// Left at 08:17:00
-
 import Image from "next/image";
-import { Expand } from "lucide-react"
+import { Expand, ShoppingCart } from "lucide-react"
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
     data: Product;
@@ -15,8 +15,15 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const router = useRouter()
+
+    const handleClick = () =>
+    {
+        router.push(`/product/${data?.id}`)
+    }
+
     return ( 
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images and Actions */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image 
@@ -31,8 +38,25 @@ const ProductCard: React.FC<ProductCard> = ({
                             onClick={() => {}}
                             icon={<Expand size={20} className="tex-gray-600"/>}
                         />
+                        <IconButton 
+                            onClick={() => {}}
+                            icon={<ShoppingCart size={20} className="tex-gray-600"/>}
+                        />
                     </div>
                 </div>
+            </div>
+            {/* Description */}
+            <div>
+                <p className="font-semibold text-lg">
+                    {data.name}
+                </p>
+                <p className="text-sm text-gray-500">
+                    {data.category?.name}
+                </p>
+            </div>
+            {/* Price */}
+            <div className="flex items-center justify-between">
+                <Currency value={data?.price}/>
             </div>
         </div>
      );
