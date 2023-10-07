35f59e5e1f9ce703c372438118be87bd1ff8d9f8
George Lungu <george.lungu1896@gmail.com>
2023-10-07 22:59:05 +0300

    created pop-up for the product selected

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+// left at 09:20:30
+
 import Image from "next/image";
 import { Expand, ShoppingCart } from "lucide-react"
+import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react"
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
-import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCard {
     data: Product;
@@ -15,11 +19,19 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const previewModal = usePreviewModal()
     const router = useRouter()
 
     const handleClick = () =>
     {
         router.push(`/product/${data?.id}`)
+    }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) =>
+    {
+        event.stopPropagation()
+
+        previewModal.onOpen(data)
     }
 
     return ( 
@@ -35,7 +47,7 @@ const ProductCard: React.FC<ProductCard> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton 
-                            onClick={() => {}}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="tex-gray-600"/>}
                         />
                         <IconButton 
