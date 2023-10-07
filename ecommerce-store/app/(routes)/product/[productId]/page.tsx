e8ba0b390f236078a989022c51ff68afc4291865
George Lungu <george.lungu1896@gmail.com>
2023-10-07 13:12:33 +0300

    created gallery section and added the buy button w/o functionality

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,9 +1,9 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
-
-// left at 08:30:35
 
 interface ProductPageProps {
     params: {
@@ -24,11 +24,9 @@ const ProductPage: React.FC<ProductPageProps> = async ({
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        {/* Gallery */}
-                        <div>Galery</div>
+                        <Gallery images={product.images}/>
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                            {/* Info */}
-                            Info
+                            <Info data={product}/>
                         </div>
                     </div>
                     <hr className="my-10"/>
